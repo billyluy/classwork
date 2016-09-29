963de5a832a32492dab071a521e28de9c7abe562
Student6 <Student6@10.8.33.156>
2016-09-29 12:36:24 -0400

    update

--- Introduction/src/chatbot/BillyHello.java
+++ Introduction/src/chatbot/BillyHello.java
@@ -3,20 +3,20 @@ package chatbot;
 import chatbot.BillyMain;
 
 public class BillyHello implements Topic {
-	
+
 	private boolean inHelloLoop;
 	private String helloResponse;
-	
+
 	private int helloCount;
-	
+
 	//responses are constants(never change)
-	private static String[] calmResponses = {"We've already said hellos. Remember?", "Yes, hello to you too. Lets actually talk."};
+	private static String[] calmResponses = {"We've already said hello. Remember?", "Yes, hello to you too. Lets actually talk."};
 	private static String[] angryResponses = {"Okay, seriously. This has to stop.", "You are beginning to annoy me. "+ "We've SAID HELLO"};
-	
+
 	public BillyHello(){
 		helloCount = 0;
 	}
-	
+
 	public void talk(){
 		inHelloLoop = true;
 		while(inHelloLoop){
@@ -28,9 +28,9 @@ public class BillyHello implements Topic {
 				BillyMain.talkForever();
 			}
 		}
-		
+
 	}
-	
+
 	private void printResponse() {
 		int responseIndex =0;
 		if(helloCount < 5){
@@ -41,7 +41,7 @@ public class BillyHello implements Topic {
 			responseIndex = (int)(Math.random() * angryResponses.length);
 			BillyMain.print(angryResponses[responseIndex]);
 		}
-		
+
 	}
 
 	public boolean isTriggered(String userInput){
--- Introduction/src/chatbot/BillyMain.java
+++ Introduction/src/chatbot/BillyMain.java
@@ -3,161 +3,161 @@ package chatbot;
 import java.util.Scanner;
 
 public class BillyMain {
-	
-		static Scanner input;
-		static String user;
-		static boolean inLoop;
-		static String response;
-		static Topic school;
-		static Topic like;
-		static Topic hello;
 
-		public static void main(String[] args) {
-			createTopics();
-			promptName();
-			talkForever();
+	static Scanner input;
+	static String user;
+	static boolean inLoop;
+	static String response;
+	static Topic school;
+	static Topic like;
+	static Topic hello;
 
-		}
-
-		public static void promptName() {
-			print("Hello, human! I am a board covered with semiconductors\n and other such electronic components, What is your name?");
-			// /n makes the line break
-			user = input.nextLine(); //user referencing the field user
-			print("Hello "+ user);
-		}
-
-		public static void talkForever() {
-			inLoop = true;
-			while(inLoop){ //what ever is inside the while loop is true, it keeps running
-				//promptInput();
-				print("Greetings, "+user+". How are you?");
-				response = getInput();
-				if (findKeyword(response, "good", 0)>=0){
-					print("I'm so happy you're good");
-				}
-				else if (like.isTriggered(response)){
-					inLoop =false;//exits the loop
-					like.talk();
-				}
-				else if (hello.isTriggered(response)){
-					inLoop =false;//exits the loop
-					hello.talk();
-				}
-				else if(school.isTriggered(response)){
-					inLoop =false;//exits the loop
-					school.talk();//calls on the school class
-				}
-				else
-					print("Im sorry i dont understand you");
-			}
-		}
-
-		public static int findKeyword(String searchString, String key, int startIndex) {
-			String phrase = searchString.trim();//deletes all the white space in the input
-			//set all letters lowerCase
-			phrase = phrase.toLowerCase();
-			key = key.toLowerCase();
-//			System.out.println("The phrase is " +phrase);
-//			System.out.println("The key is " +key);
-			int psn = phrase.indexOf(key); //finds the first position of keep word
-			
-//			System.out.println("The position is " +psn);
-			
-			//keep checking for key word until context is found
-			while (psn>=0){ //only runs if the word is found
-				//checks to see if the key is isolated
-				String before=" ";
-				String after = " ";
-				if(psn + key.length() <phrase.length()){ //checks the character after the key
-					//substring returns part of the string
-					after = phrase.substring(psn + key.length(), psn + key.length()+1);
-				}
-				if(psn>0){ //checks the character before the key
-					before = phrase.substring(psn-1,psn);
-				}
-				if(before.compareTo("a")<0 && after.compareTo("a")<0){
-					if(noNegation(phrase, psn)){
-						return psn;
-					}
-					return -1;
-				}
-				//if keyword is not found yet, check the rest of the string
-				psn = phrase.indexOf(key,psn+1);
-			}
-			
-			return -1; //return -1 if word is not found
-		}
-		
-		/*"help method" a method that contributes functionality to another method
-		 * Make methods more readable
-		 * This method is private  because it's only used by the method it is helping
-		 */
-
-		private static boolean noNegation(String phrase, int index) {
-			//check for word "NO " - 3 characters long
-			//check to see if there is space for the word "NO " to be in front of index
-			if(index - 3 >= 0 && phrase.substring(index -3, index).equals("no ")){
-				return false;
-			}
-			//check for word "NOT " - 4 characters long
-			if(index - 4 >= 0 && phrase.substring(index -4, index).equals("not ")){
-				return false;
-			}
-			//check for word "NEVER  " - 6 characters long
-			if(index - 6 >= 0 && phrase.substring(index -6, index).equals("never ")){
-				return false;
-			}
-			//check for word "N'T  " - 4 characters long
-			if(index - 4 >= 0 && phrase.substring(index -4, index).equals("n't ")){
-				return false;
-			}
-			return true;
-		}
-
-		public static void promptInput() {
-			print(user+" Try inputing a string");
-			String userInput = input.nextLine();
-			print("You typed "+userInput);
-
-		}
-		
-		public static String getInput(){
-			return input.nextLine();
-		}
-
-		public static void print(String s){
-			String printString = "";
-			//loop to count the worlds per line
-			int cutOff = 35;
-			while(s.length() > 0){
-				String currentLine = "";
-				String nextWord = "";
-				//while the current line and next word are less than 35(cutoff) & there are still words to add, do the following
-				while(currentLine.length()+nextWord.length()<=cutOff && s.length() > 0){
-					//add next word to the line
-					currentLine += nextWord;
-					//remove the word added
-					s = s.substring(nextWord.length());
-					int endOfWord = s.indexOf(" "); //check to see if this is last word
-					if(endOfWord == -1){
-						endOfWord = s.length()-1;
-					}
-					nextWord = s.substring(0,endOfWord+1);
-				}
-				printString += currentLine + "\n";
-			}
-			System.out.println(printString);
-			//		lineCount++ ;
-			//		print("Line #: " + lineCount+" "+s);//don't need this line, can use print() instead
-		}
-
-
-		public static void createTopics() {
-			input = new Scanner(System.in);//allows for user input
-			school = new School();
-			like = new BillyLike();
-			hello = new BillyHello();
-
-		}
+	public static void main(String[] args) {
+		createTopics();
+		promptName();
+		talkForever();
 
 	}
+
+	public static void promptName() {
+		print("Hello, human! I am a board covered with semiconductors\n and other such electronic components, What is your name?");
+		// /n makes the line break
+		user = input.nextLine(); //user referencing the field user
+		print("Hello "+ user);
+	}
+
+	public static void talkForever() {
+		inLoop = true;
+		while(inLoop){ //what ever is inside the while loop is true, it keeps running
+			//promptInput();
+			print("Greetings, "+user+". How are you?");
+			response = getInput();
+			if (findKeyword(response, "good", 0)>=0){
+				print("I'm so happy you're good");
+			}
+			else if (like.isTriggered(response)){
+				inLoop =false;//exits the loop
+				like.talk();
+			}
+			else if (hello.isTriggered(response)){
+				inLoop =false;//exits the loop
+				hello.talk();
+			}
+			else if(school.isTriggered(response)){
+				inLoop =false;//exits the loop
+				school.talk();//calls on the school class
+			}
+			else
+				print("Im sorry i dont understand you");
+		}
+	}
+
+	public static int findKeyword(String searchString, String key, int startIndex) {
+		String phrase = searchString.trim();//deletes all the white space in the input
+		//set all letters lowerCase
+		phrase = phrase.toLowerCase();
+		key = key.toLowerCase();
+		//			System.out.println("The phrase is " +phrase);
+		//			System.out.println("The key is " +key);
+		int psn = phrase.indexOf(key); //finds the first position of keep word
+
+		//			System.out.println("The position is " +psn);
+
+		//keep checking for key word until context is found
+		while (psn>=0){ //only runs if the word is found
+			//checks to see if the key is isolated
+			String before=" ";
+			String after = " ";
+			if(psn + key.length() <phrase.length()){ //checks the character after the key
+				//substring returns part of the string
+				after = phrase.substring(psn + key.length(), psn + key.length()+1);
+			}
+			if(psn>0){ //checks the character before the key
+				before = phrase.substring(psn-1,psn);
+			}
+			if(before.compareTo("a")<0 && after.compareTo("a")<0){
+				if(noNegation(phrase, psn)){
+					return psn;
+				}
+				return -1;
+			}
+			//if keyword is not found yet, check the rest of the string
+			psn = phrase.indexOf(key,psn+1);
+		}
+
+		return -1; //return -1 if word is not found
+	}
+
+	/*"help method" a method that contributes functionality to another method
+	 * Make methods more readable
+	 * This method is private  because it's only used by the method it is helping
+	 */
+
+	private static boolean noNegation(String phrase, int index) {
+		//check for word "NO " - 3 characters long
+		//check to see if there is space for the word "NO " to be in front of index
+		if(index - 3 >= 0 && phrase.substring(index -3, index).equals("no ")){
+			return false;
+		}
+		//check for word "NOT " - 4 characters long
+		if(index - 4 >= 0 && phrase.substring(index -4, index).equals("not ")){
+			return false;
+		}
+		//check for word "NEVER  " - 6 characters long
+		if(index - 6 >= 0 && phrase.substring(index -6, index).equals("never ")){
+			return false;
+		}
+		//check for word "N'T  " - 4 characters long
+		if(index - 4 >= 0 && phrase.substring(index -4, index).equals("n't ")){
+			return false;
+		}
+		return true;
+	}
+
+	public static void promptInput() {
+		print(user+" Try inputing a string");
+		String userInput = input.nextLine();
+		print("You typed "+userInput);
+
+	}
+
+	public static String getInput(){
+		return input.nextLine();
+	}
+
+	public static void print(String s){
+		String printString = "";
+		//loop to count the worlds per line
+		int cutOff = 35;
+		while(s.length() > 0){
+			String currentLine = "";
+			String nextWord = "";
+			//while the current line and next word are less than 35(cutoff) & there are still words to add, do the following
+			while(currentLine.length()+nextWord.length()<=cutOff && s.length() > 0){
+				//add next word to the line
+				currentLine += nextWord;
+				//remove the word added
+				s = s.substring(nextWord.length());
+				int endOfWord = s.indexOf(" "); //check to see if this is last word
+				if(endOfWord == -1){
+					endOfWord = s.length()-1;
+				}
+				nextWord = s.substring(0,endOfWord+1);
+			}
+			printString += currentLine + "\n";
+		}
+		System.out.println(printString);
+		//		lineCount++ ;
+		//		print("Line #: " + lineCount+" "+s);//don't need this line, can use print() instead
+	}
+
+
+	public static void createTopics() {
+		input = new Scanner(System.in);//allows for user input
+		school = new School();
+		like = new BillyLike();
+		hello = new BillyHello();
+
+	}
+
+}
