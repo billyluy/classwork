14afe21a53cdb5ad13edf430d84b7e2c4aec5534
Student6 <Student6@10.8.33.156>
2016-10-24 11:53:40 -0400

    update

--- Introduction/src/array/ArrayMethods.java
+++ Introduction/src/array/ArrayMethods.java
@@ -6,7 +6,22 @@ public class ArrayMethods {
 		int[] arr = {2,3,4,6,9,11,12,15};
 		checkHalfway(arr,12,0, arr.length-1);
 		swap(arr, 0, arr.length-1);
+		shuffle(arr);
+		print(arr);
 
+	}
+private static void print(int[] arr) {
+		for(int i = 0; i < arr.length-1; i++){
+			System.out.print(arr[i] +", ");
+		}
+		System.out.println(arr[arr.length-1]);
+		
+	}
+private static void shuffle(int[] arr) {
+		for(int i= 0; i< arr.length; i++){
+			int random = (int)(Math.random()*arr.length);
+			swap(arr, i , random);
+		}
 	}
 private static void swap(int[] arr, int i, int j) {
 		int placeholder = arr[j];
