76668a6f1519a2ee5563bed701564e20d1b200f7
Student6 <Student6@10.8.33.156>
2016-12-02 13:19:01 -0500

    up

--- Introduction/src/sort/MainBattle.java
+++ Introduction/src/sort/MainBattle.java
@@ -8,12 +8,28 @@ public class MainBattle {
 		raticate.iChooseYou();
 		bulbasaur.iChooseYou();
 		System.out.println("Raticate is preparing to attack with super fang");
+		raticate.attack(bulbasaur,new Attack() {
+			public void attack(Pokemon target) {
+				System.out.println("Superfang is used!");
+				int newHp = target.getHp()/2;
+				target.setHP(newHp);
+			}
+		});
+		bulbasaur.attack(raticate, new Attack() {
+			public void attack(Pokemon target) {
+				System.out.println("Toxic is used!");
+				target.setPoisoned(true);
+			}
+		});
 		//create a way for raticate and bulbasaur to attack
 		raticate.lapse();
 		bulbasaur.lapse();
 		printScore(raticate, bulbasaur);
 	}
 	
-	private static void
+	private static void printScore(Pokemon p1, Pokemon p2){
+		System.out.println(p1.getName()+", Hp ="+p1.getHp());
+		System.out.println(p2.getName()+", Hp ="+p2.getHp());
+	}
 
 }
